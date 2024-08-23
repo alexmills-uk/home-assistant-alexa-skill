a88fbad4f3430f2b9f9f1bab6580ec1574a505d2
Alex Mills <alex@alexmills.uk>
2024-08-23 19:53:27 +0000

    fixed variable names

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "HOME_ASSISTANT_BASE_URL" {
+variable "home_assistant_base_url" {
   sensitive   = true
   description = "The base URL to your home assistant installation. Must be Internet-facing and include the protocol."
   type        = string
 }
 
-variable "LONG_LIVED_ACCESS_TOKEN" {
+variable "long_lived_access_token" {
   sensitive   = true
   type        = string
   description = "A long-lived access token to allow the Lambda function to authenticate with your Home Assistant instance."
