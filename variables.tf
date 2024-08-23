520261dd9fa957c9e0b638aea557f34f31294b4e
Alex Mills <alex@alexmills.uk>
2024-08-23 19:35:08 +0000

    made variables sensitive

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "HOME_ASSISTANT_BASE_URL" {
+  sensitive   = true
   description = "The base URL to your home assistant installation. Must be Internet-facing and include the protocol."
   type        = string
 }
 
 variable "LONG_LIVED_ACCESS_TOKEN" {
+  sensitive   = true
   type        = string
   description = "A long-lived access token to allow the Lambda function to authenticate with your Home Assistant instance."
 }
