5e0d4fe1559ac69f480133ad74b6a0e3a185b4ab
Alex Mills <alex@alexmills.uk>
2024-08-23 20:27:27 +0000

    corrected principal for alexa smart home trigger

--- main.tf
+++ main.tf
@@ -44,6 +44,6 @@ resource "aws_lambda_permission" "alexa" {
   statement_id       = "AllowExecutionFromAlexa"
   action             = "lambda:InvokeFunction"
   function_name      = module.alexa_home_assistant.lambda_function_name
-  principal          = "alexa-appkit.amazon.com"
+  principal          = "alexa-connectedhome.amazon.com"
   event_source_token = var.alexa_skill_id
 }
